e189bdf03f20ae8e467c0a1054cc4279cb280738
EDUARDO ALVES LEONARDO <082170006@ftt.cefsa.edu.br>
2020-02-17 21:46:47 -0300

    finished: calculator in Java language

--- src/starterjava/StarterJava.java
+++ src/starterjava/StarterJava.java
@@ -1,20 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package starterjava;
+
+import java.util.Scanner;
 
 /**
  *
  */
 public class StarterJava {
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Digite um número inteiro: ");
+	Scanner scan = new Scanner(System.in);
+		
+	int x = scan.nextInt();
+		
+	for(int i = 1; i <= 10; i++) {
+            int r = x * i;
+            System.out.println(x+" x "+i+" = "+r);
+	}
+		
+	System.out.println("Tabuada do número " + x + " finalizada!");
+	scan.close();
     }
-    
 }
